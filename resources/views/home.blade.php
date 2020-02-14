251f3447cce95423e3757b7bc10fabc2e4a9353e
maher <ba.maher94@gmail.com>
2020-02-14 08:11:55 +0100

    home page dev + some styles change

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,20 +2,22 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img
+                src="https://instagram.ftun9-1.fna.fbcdn.net/v/t51.2885-19/s150x150/60468629_2071308019658649_8105413550312259584_n.jpg?_nc_ht=instagram.ftun9-1.fna.fbcdn.net&_nc_ohc=eI3Bh8aiSlgAX9OisCk&oh=e9f7da60f361a4619f7f489de52e6889&oe=5EC75F9C"
+                alt="" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div>
+                <h1>
+                    tahaliasavage
+                </h1>
+            </div>
+            <div class="d-flex">
+                <div class="pr-3"><strong>123</strong> followors</div>
+                <div class="pr-3"><strong>34</strong> posts</div>
+                <div class="pr-3"><strong>44</strong> following</div>
             </div>
         </div>
     </div>
