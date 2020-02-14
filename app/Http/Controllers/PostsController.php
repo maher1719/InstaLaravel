5604d58b1cd4169ce8e9e3a3bf8cb57d70cf37a1
maher <ba.maher94@gmail.com>
2020-02-14 23:56:49 +0100

    more dev posts form

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
+
 class PostsController extends Controller
 {
     //
     public function create()
     {
         return view("posts.create");
+    }
+
+    public function store()
+    {
+        $data = request()->validate([
+            'caption' => 'required',
+            'image' => ['required', 'image'],
+        ]);
+        Post::create($data);
+        dd(request()->all());
+        //return view("posts.create");
     }
 
 }
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,31 +2,37 @@
 
 @section('content')
     <div class="container">
-        <form action="">
+        <div class="row">
+
+        </div>
+        <form action="/p" enctype="multipart/form-data" method="post">
+            @csrf
             <div class="row">
                 <div class="col-8 offset-2">
+                    <div class="row">
+                        <h1>Add new post</h1>
+                    </div>
                     <div class="form-group row">
-                        <label for="caption" class="col-md-4 col-form-label text-md-right">Image caption</label>
-
+                        <label for="caption" class="col-md-4 col-form-label">Image caption</label>
 
                         <input id="caption" type="text" class="form-control @error('caption') is-invalid @enderror"
                                name="caption" value="{{ old('caption') }}" autocomplete="caption" autofocus>
 
                         @error('caption')
-                        <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+
+                        <strong>{{ $message }}</strong>
+
                         @enderror
 
 
                     </div>
                     <div class="form-group row">
-                        <label for="image" class="col-md-4 col-form-label text-md-right">fichier image</label>
+                        <label for="image" class="col-md-4 col-form-label">fichier image</label>
                         <input type="file" class="form-control-file" name="image" id="image">
                         @error('image')
-                        <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+
+                        <strong>{{ $message }}</strong>
+
                         @enderror
                     </div>
                     <div class="row">
